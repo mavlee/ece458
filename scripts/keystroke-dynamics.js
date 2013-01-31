c06d2bd947031cc97ec0afc9c72965260128ba05
Maverick <maverick.lee225@gmail.com>
2013-01-31 09:43:30 -0500

    get timing data correctly calculate mean and stddev

--- scripts/keystroke-dynamics.js
+++ scripts/keystroke-dynamics.js
@@ -1,5 +1,6 @@
 data = {}
 calculatedData = {};
+timingData = {};
 cTime = null;
 cChar = null;
 pChar = null;
@@ -20,6 +21,11 @@ $('#displayData').click(function() {
 
 $('#reset').click(function() {
   data = {};
+  timingData = {};
+  cTime = null;
+  cChar = null;
+  pChar = null;
+  pTime = null;
   $('#p1').val('');
   $('#p2').val('');
   $('#p3').val('');
@@ -28,8 +34,8 @@ $('#reset').click(function() {
 });
 
 $('#p1,#p2,#p3,#p4,#p5').keydown(function(e) {
-  // ignore tabs
-  if (e.keyCode == 9) {
+  // ignore tabs and shift keys
+  if (e.keyCode == 9 || e.keyCode == 16) {
     cChar = null;
     pChar = null;
     return;
@@ -38,6 +44,7 @@ $('#p1,#p2,#p3,#p4,#p5').keydown(function(e) {
   q = new Date();
   cTime = q.getTime();
   cChar = e.keyCode;
+  timingData[convert(cChar)] = cTime;
   if (pChar != null) {
     seq = convert(pChar) + "-" + convert(cChar);
     if (data[seq] == null) {
@@ -45,11 +52,14 @@ $('#p1,#p2,#p3,#p4,#p5').keydown(function(e) {
     }
     data[seq].push(cTime - pTime);
   }
+  pChar = cChar;
+  pTime = cTime;
+  //console.log(convert(cChar) + " key down time: " + cTime);
 });
 
 $('#p1,#p2,#p3,#p4,#p5').keyup(function(e) {
-  // ignore tabs
-  if (e.keyCode == 9) {
+  // ignore tabs and shift keys
+  if (e.keyCode == 9 || e.keyCode == 16) {
     cChar = null;
     pChar = null;
     return;
@@ -57,15 +67,33 @@ $('#p1,#p2,#p3,#p4,#p5').keyup(function(e) {
 
   q = new Date();
   w = q.getTime();
-  ch = convert(cChar);
+  ch = convert(e.keyCode);
   if (data[ch] == null) {
     data[ch] = [];
   }
-  data[ch].push(w - cTime);
-  pChar = cChar;
-  pTime = w;
+  data[ch].push(w - timingData[ch]);
+  //console.log(convert(e.keyCode) + " key up time: " + w);
 });
 
 $('#calculate').click(function() {
+  for (x in data) {
+    var values = data[x];
+    timingData[x] = {};
+    var total = 0;
 
+    // get average
+    for (var i = 0; i < data[x].length; i++) {
+      total += data[x][i];
+    }
+    timingData[x]['average'] = total / data[x].length;
+    timingData[x]['datapoints'] = data[x].length;
+
+    // get std dev
+    var stdDevTotal = 0;
+    for (var i = 0; i < data[x].length; i++) {
+      stdDevTotal += (data[x][i] - timingData[x]['average']) * (data[x][i] - timingData[x]['average']);
+    }
+    timingData[x]['stddev'] = Math.sqrt(stdDevTotal / data[x].length);
+  }
+  console.log(timingData);
 });
