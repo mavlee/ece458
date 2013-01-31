bd8cf406b97c9745b3f1c6ec6991fb1ac6bd00a8
Maverick <maverick.lee225@gmail.com>
2013-01-30 23:32:48 -0500

    add in base code

---
+++ scripts/keystroke-dynamics.js
@@ -0,0 +1,52 @@
+data = {}
+cTime = null;
+cChar = null;
+pChar = null;
+pTime = null;
+
+function convert(pChar) {
+  charDisp = String.fromCharCode(pChar);
+  if (pChar == 8)
+    charDisp = "backspace";
+  else if (pChar == 32)
+    charDisp = "space";
+  return charDisp;
+}
+
+$('#displayData').click(function() {
+  console.log(data);
+});
+
+$('#reset').click(function() {
+  data = {};
+  $('#p1').val('');
+  $('#p2').val('');
+  $('#p3').val('');
+  $('#p4').val('');
+  $('#p5').val('');
+});
+
+$('#p1,#p2,#p3,#p4,#p5').keydown(function(e) {
+  q = new Date();
+  cTime = q.getTime();
+  cChar = e.keyCode;
+  if (pChar != null) {
+    seq = convert(pChar) + "-" + convert(cChar);
+    if (data[seq] == null) {
+      data[seq] = [];
+    }
+    data[seq].push(cTime - pTime);
+  }
+});
+
+$('#p1,#p2,#p3,#p4,#p5').keyup(function(e) {
+  q = new Date();
+  w = q.getTime();
+  ch = convert(cChar);
+  if (data[ch] == null) {
+    data[ch] = [];
+  }
+  data[ch].push(w - cTime);
+  pChar = cChar;
+  pTime = w;
+});
